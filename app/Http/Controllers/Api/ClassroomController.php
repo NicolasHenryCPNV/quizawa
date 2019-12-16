bf490d18121a41da6966085fb5c395839477cde1
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-12-16 11:34:45 +0100

    Add classrooms api

---
+++ app/Http/Controllers/Api/ClassroomController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Classroom;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ClassroomCollection;
+use App\Http\Resources\Classrooms;
+use Illuminate\Http\Request;
+
+class ClassroomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new ClassroomCollection(Classroom::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Classroom  $classroom
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Classroom $classroom)
+    {
+        return new Classrooms($classroom);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Classroom  $classroom
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Classroom $classroom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Classroom  $classroom
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Classroom $classroom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Classroom  $classroom
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Classroom $classroom)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 Route::apiResource('users', 'Api\UserController');
 Route::apiResource('quizzes', 'Api\QuizzController');
 Route::apiResource('questions', 'Api\QuestionController');
+Route::apiResource('classrooms', 'Api\ClassroomController');
 
 /* 
 Inscription
