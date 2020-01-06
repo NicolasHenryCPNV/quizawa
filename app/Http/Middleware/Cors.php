3d78431e200074f52a241cd97b4cb4de1f6d2bb0
JUNOD Alexandre <Alexandre.JUNOD@cpnv.ch>
2020-01-06 11:05:39 +0100

    Change cors handle content

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,10 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access - Control - Allow - Origin', ' * ')
-            ->header('Access - Control - Allow - Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access - Control - Allow - Headers', 'X - Requested - With, Content - Type, X - Token - Auth, Authorization');
+        $response = $next( $request );
+        $response->header( 'Access-Control-Allow-Origin', '*' );
+        $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type' );
+
+        return $response;
     }
 }
