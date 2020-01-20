3fed08968f13d05d79b851183aca30841c8d8e86
Nicolas Henry <nicolas.henry@cpnv.ch>
2020-01-20 11:20:05 +0100

    Add answers swagger

--- app/Http/Controllers/Api/AnswerController.php
+++ app/Http/Controllers/Api/AnswerController.php
@@ -9,8 +9,38 @@ use App\Http\Resources\Answers;
 use App\Question;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Tag(
+ *      name="Answers",
+ *      description="Answers APIs",
+ * )
+ */
 class AnswerController extends Controller
 {
+    /**
+     * @OA\GET(
+     *      path="/api/answers",
+     *      tags={"Answers"},
+     *      description="List of answers",
+     *      @OA\Parameter(
+     *          name="api_token",
+     *          in="query",
+     *          description="User authentication",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      )
+     * )
+     */
+
     /**
      * Display a listing of the resource.
      *
@@ -32,6 +62,64 @@ class AnswerController extends Controller
     }
 
     /**
+     * @OA\POST(
+     *      path="/api/answers",
+     *      tags={"Answers"},
+     *      description="Store an answer",
+     *      @OA\Parameter(
+     *          name="api_token",
+     *          in="query",
+     *          description="User authentication",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="value",
+     *          in="query",
+     *          description="Value of answer",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="correct",
+     *          in="query",
+     *          description="if he's correct",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="question_id",
+     *          in="query",
+     *          description="id of the question",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="stored",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="parameters are missing",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      )
+     * )
+     */
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -47,6 +135,46 @@ class AnswerController extends Controller
         
         return response()->json($answer, 201);
     }
+
+    /**
+     * @OA\GET(
+     *      path="/api/answers/{answer}",
+     *      tags={"Answers"},
+     *      description="Show one answer",
+     *      @OA\Parameter(
+     *          name="api_token",
+     *          in="query",
+     *          description="User authentication",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="answer",
+     *          in="path",
+     *          description="ID of the answer to return",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="parameters are missing",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      )
+     * )
+     */
 
     /**
      * Display the specified resource.
@@ -71,6 +199,80 @@ class AnswerController extends Controller
     }
 
     /**
+     * @OA\PATCH(
+     *      path="/api/answers/{answer}",
+     *      tags={"Answers"},
+     *      description="List of questions for a answer",
+     *      @OA\Parameter(
+     *          name="api_token",
+     *          in="query",
+     *          description="User authentication",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="value",
+     *          in="query",
+     *          description="Value of answer",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="correct",
+     *          in="query",
+     *          description="if he's correct",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="question_id",
+     *          in="query",
+     *          description="id of the question",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="answer",
+     *          in="path",
+     *          description="ID of the answer to update",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="parameters are missing",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="not found",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      )
+     * )
+     */
+
+    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -85,6 +287,53 @@ class AnswerController extends Controller
 
         return response()->json($answer, 200);
     }
+
+    /**
+     * @OA\DELETE(
+     *      path="/api/answers/{answer}",
+     *      tags={"Answers"},
+     *      description="delete one answer",
+     *      @OA\Parameter(
+     *          name="api_token",
+     *          in="query",
+     *          description="User authentication",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="answer",
+     *          in="path",
+     *          description="ID of the answer to delete",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="parameters are missing",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="not found",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      )
+     * )
+     */
 
     /**
      * Remove the specified resource from storage.
